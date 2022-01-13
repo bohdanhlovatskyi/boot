bc755881e5d9e7b195f669f46bea9f389a2ff118
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2022-01-13 14:17:45 +0200

    booted with deletion

--- loader/loader.py
+++ loader/loader.py
@@ -3,6 +3,8 @@ import serial.tools.list_ports as lp
 import time
 import os
 
+file_name = "boot_pr_example.bin"
+
 if __name__ == "__main__":
 
     port = [port.name for port in lp.comports() if "usbserial" in port.name][0]
@@ -14,12 +16,12 @@ if __name__ == "__main__":
         # ser.write(bytes([48]))
 
         bw = 0
-        with open("FIRMWARE.bin", "rb") as f:
+        with open(file_name, "rb") as f:
             while (chunk := f.read(4)):
                 ser.write(chunk)
                 bw += 4
                 time.sleep(0.05)
-                print(f'{bw} / {os.path.getsize("FIRMWARE.bin")}')
+                print(f'{bw} / {os.path.getsize(file_name)}')
 
         b = bytearray()
         b.extend(map(ord, "$END"))
